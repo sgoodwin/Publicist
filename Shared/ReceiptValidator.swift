cc9828c44410ba9d9c881091302bda9919417ec6
Samuel Goodwin <samuel@roundwallsoftware.com>
2021-02-26 12:51:05 +0100

    Ready to ship!
    I fixed the receipt validation to use something arch-friendly.
    Also added in app icons.

--- Shared/ReceiptValidator.swift
+++ Shared/ReceiptValidator.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-import AppReceiptValidator
+import TPInAppReceipt
 
 enum ValdationResult {
     case success
@@ -32,37 +32,21 @@ struct ReceiptValidator {
         }
         
         let data = try Data(contentsOf: url)
+        let receipt = try InAppReceipt.receipt(from: data)
+        try receipt.verify()
         
-        let validator = AppReceiptValidator()
-        let parameters = AppReceiptValidator.Parameters.default.with {
-            $0.receiptOrigin = .data(data)
-            $0.signatureValidation = .skip
+        if !receipt.hasPurchases {
+            return .new
         }
-        let validation = validator.validateReceipt(parameters: parameters)
-        switch validation {
-        case .success(let receipt, _, _):
-            if receipt.inAppPurchaseReceipts.count == 0 {
-                return .new
-            }
-            
-            let purchases = receipt.inAppPurchaseReceipts.filter { (inAppPurchase) -> Bool in
-                if inAppPurchase.productIdentifier == "com.roundwallsoftware.Publisher.Yearly" {
-                    // We don't care anymore if the subscription is expired or not
-                    return true
-                }
-                if inAppPurchase.productIdentifier == "com.roundwallsoftware.Publisher.UnlockFroever" {
-                    return true
-                }
-                      
-                return true
-            }
-            if purchases.count == 0 {
-                return .expired
-            }
-            
-            return .success
-        case .error:
-            return .invalid
+        
+        let purchases = receipt.purchases.filter({
+            return $0.productIdentifier == "com.roundwallsoftware.Publisher.Yearly" || $0.productIdentifier == "com.roundwallsoftware.Publisher.UnlockFroever"
+        })
+        
+        if purchases.count == 0 {
+            return .expired
         }
+        
+        return .success
     }
 }
